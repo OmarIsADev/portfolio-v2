e68cd32e78c296cf4956b363b67f691466c10fd4
omar <saadomar343@gmail.com>
2025-03-09 22:52:16 +0200

    mobile thesis ui fixed

--- src/sections/thesis.jsx
+++ src/sections/thesis.jsx
@@ -15,9 +15,9 @@ function Hero() {
   return (
     <div className="col-span-2 md:row-span-1 row-span-3 flex items-center justify-center">
       <img src={ThesisImg} alt="" className="absolute z-0 md:p-0 p-3" />
-      <div className="z-10 flex flex-col items-start gap-2.5">
-        <h1>Welcome!</h1>
-        <h3>
+      <div className="z-10 flex flex-col items-start gap-2.5 max-sm:items-center">
+        <h1 className="max-sm:text-7xl">Welcome!</h1>
+        <h3 className="w-fit max-sm:max-w-[80vw]">
           My name is Omar,
           <br /> I&apos;m a Fullstack web Developer.
         </h3>
