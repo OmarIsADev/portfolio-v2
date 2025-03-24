85fe833519de088838fb3336bfe2da2956cc55ce
omar saad <omarisadev@gmail.com>
2025-03-24 23:58:56 +0200

    Thesis and about me changed, techs add description

--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -1,11 +1,17 @@
-export default function Tech({title, image, description}) {
+export default function Tech({ title, image, summary, description }) {
   return (
-    <div className="flex flex-row gap-6 w-full h-24 items-center border border-[var(--primary)] rounded-lg px-4 py-2">
-        <img className="col-span-1 h-16 w-16" src={image} alt={title} />
-        <div className="col-span-3 flex flex-col">
-            <h2 className="text-xl">{title}</h2>
-            <p className="text-sm">{description}</p>
+    <div id={title} className="group flex h-24 w-full flex-row items-center gap-6 rounded-lg border border-[var(--primary)] px-4 py-2 relative">
+      <img className="col-span-1 h-16 w-16" src={image} alt={title} />
+      <div className="col-span-3 flex w-full flex-col">
+        <h2 className="text-xl">{title}</h2>
+        <p className="text-sm">{summary}</p>
+        <div
+          style={{ width: document.getElementById(title)?.clientWidth }}
+          className="absolute top-[calc(100%+10px)] z-10 h-0 overflow-hidden rounded-md border left-0 border-zinc-600 bg-[var(--primary)] p-0 opacity-0 transition-opacity group-hover:h-fit group-hover:p-4 group-hover:opacity-100"
+        >
+          <p>{description}</p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
